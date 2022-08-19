f2a9f1f4e70840822c18c4ab31829135c06487e2
likevishal87 <likevishalverma@gmail.com>
2022-08-19 21:43:18 +0530

    coin

---
+++ Flipcoin.java
@@ -0,0 +1,13 @@
+package flip;
+import java.util.Random;
+
+public class Flipcoin {
+   public static void main(String[] args) {
+	   if (Math.random() < 0.5){
+           System.out.println("Heads");
+   }
+	   else{
+           System.out.println("Tails");
+   }
+   }
+}
