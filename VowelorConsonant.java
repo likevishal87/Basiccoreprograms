7f15951487fd85a3904b60052cf8ed0a7ebda74c
likevishal87 <likevishalverma@gmail.com>
2022-08-21 14:47:58 +0530

    Vowel or Consonant

---
+++ VowelorConsonant.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VowelorConsonant {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any char");
+		String x = sc.next();
+		if (x.equalsIgnoreCase("A") || x.equalsIgnoreCase("E") || x.equalsIgnoreCase("I") ||
+				x.equalsIgnoreCase("O") || x.equalsIgnoreCase("U"))
+  System.out.println("Vowel");
+else
+  System.out.println("Consonant");
+	}
+
+}
