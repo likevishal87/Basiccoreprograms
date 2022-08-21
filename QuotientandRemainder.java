96b92a0c3ee57ca8feef261bba3b9a772322d494
likevishal87 <likevishalverma@gmail.com>
2022-08-21 15:36:08 +0530

    Quotient and Remainder

---
+++ QuotientandRemainder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class QuotientandRemainder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int rem = num %10;
+		int quo = num /10;
+		System.out.println(rem +" "+ quo );
+	}
+
+}
